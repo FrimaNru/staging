a72355c9f917eb889104d0733cb7fc8cfe525c1f
FrimaNru <100356059+FrimaNru@users.noreply.github.com>
2024-09-05 23:48:14 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -18,14 +18,16 @@ export default function Home() {
         <link rel="apple-touch-icon" sizes="144x144" href="/faviconsWithBg/apple-icon-144x144.png" />
         <link rel="apple-touch-icon" sizes="152x152" href="/faviconsWithBg/apple-icon-152x152.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/faviconsWithBg/apple-icon-180x180.png" />
+
         <link rel="icon" type="image/png" sizes="192x192" href="/faviconsNoBg/android-icon-192x192.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/faviconsNoBg/favicon=32x32.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/faviconsNoBg/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="96x96" href="/faviconsNoBg/favicon-96x96.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/faviconsNoBg/favicon-16x16.png" />
         <link rel="manifest" href="/faviconsNoBg/manifest.json" />
+
         <meta name="msapplication-TileColor" content="#EEEEEE" />
         <meta name="msapplication-TileImage" content="/faviconsNoBg/ms-icon-144x144.png" />
-        <meta name="theme-color" content="#f==EE"></meta>
+        <meta name="theme-color" content="#EEEEEE" />
       </Head>
       <center>
         <main>
