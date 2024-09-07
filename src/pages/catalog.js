c4311b609dfb0ed95d2f23d9bf527ad2c9dca86a
FrimaNru <100356059+FrimaNru@users.noreply.github.com>
2024-09-07 16:50:53 +0300

    upd favicon on all pages

--- src/pages/catalog.js
+++ src/pages/catalog.js
@@ -9,7 +9,25 @@ export default function Delivery() {
                 <title>Каталог Ювелирных Изделий – Mi Alegria</title>
                 <meta name="description" content='Откройте для себя наш каталог ювелирных изделий: золотые и серебряные кольца, серьги, браслеты и подвески. Найдите идеальное украшение на любой случай!' />
                 <meta name="viewport" content="width=device-width, initial-scale=1" />
-                <link rel="icon" href="./favicon.ico" />
+                <link rel="apple-touch-icon" sizes="57x57" href="/faviconsWithBg.ico/apple-icon-57x57.png" />
+                <link rel="apple-touch-icon" sizes="60x60" href="/faviconsWithBg.ico/apple-icon-60x60.png" />
+                <link rel="apple-touch-icon" sizes="72x72" href="/faviconsWithBg.ico/apple-icon-72x72.png" />
+                <link rel="apple-touch-icon" sizes="76x76" href="/faviconsWithBg.ico/apple-icon-76x76.png" />
+                <link rel="apple-touch-icon" sizes="114x114" href="/faviconsWithBg.ico/apple-icon-114x114.png" />
+                <link rel="apple-touch-icon" sizes="120x120" href="/faviconsWithBg.ico/apple-icon-120x120.png" />
+                <link rel="apple-touch-icon" sizes="144x144" href="/faviconsWithBg.ico/apple-icon-144x144.png" />
+                <link rel="apple-touch-icon" sizes="152x152" href="/faviconsWithBg.ico/apple-icon-152x152.png" />
+                <link rel="apple-touch-icon" sizes="180x180" href="/faviconsWithBg.ico/apple-icon-180x180.png" />
+
+                <link rel="icon" type="image/png" sizes="192x192" href="/faviconsNoBg/android-icon-192x192.png" />
+                <link rel="icon" type="image/png" sizes="32x32" href="/faviconsNoBg/favicon-32x32.png" />
+                <link rel="icon" type="image/png" sizes="96x96" href="/faviconsNoBg/favicon-96x96.png" />
+                <link rel="icon" type="image/png" sizes="16x16" href="/faviconsNoBg/favicon-16x16.png" />
+                <link rel="manifest" href="/faviconsNoBg/manifest.json" />
+
+                <meta name="msapplication-TileColor" content="#EEEEEE" />
+                <meta name="msapplication-TileImage" content="/faviconsNoBg/ms-icon-144x144.png" />
+                <meta name="theme-color" content="#EEEEEE" />
             </Head>
             <center>
                 <main>
