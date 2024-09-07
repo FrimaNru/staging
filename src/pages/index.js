61df9a63b5b9a572af6f148cac6e62d8e89776e3
FrimaNru <100356059+FrimaNru@users.noreply.github.com>
2024-09-07 12:33:36 +0300

    upd

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,15 +9,15 @@ export default function Home() {
         <title>Mi Alegria</title>
         <meta name="description" content='Добро пожаловать в Mi Alegria! Широкий выбор эксклюзивных ювелирных украшений: кольца, серьги, браслеты и подвески. Бесплатная доставка по России.' />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="apple-touch-icon" sizes="57x57" href="/faviconsWithBg/apple-icon-57x57.png" />
-        <link rel="apple-touch-icon" sizes="60x60" href="/faviconsWithBg/apple-icon-60x60.png" />
-        <link rel="apple-touch-icon" sizes="72x72" href="/faviconsWithBg/apple-icon-72x72.png" />
-        <link rel="apple-touch-icon" sizes="76x76" href="/faviconsWithBg/apple-icon-76x76.png" />
-        <link rel="apple-touch-icon" sizes="114x114" href="/faviconsWithBg/apple-icon-114x114.png" />
-        <link rel="apple-touch-icon" sizes="120x120" href="/faviconsWithBg/apple-icon-120x120.png" />
-        <link rel="apple-touch-icon" sizes="144x144" href="/faviconsWithBg/apple-icon-144x144.png" />
-        <link rel="apple-touch-icon" sizes="152x152" href="/faviconsWithBg/apple-icon-152x152.png" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/faviconsWithBg/apple-icon-180x180.png" />
+        <link rel="apple-touch-icon" sizes="57x57" href="/faviconsWithBg.ico/apple-icon-57x57.png" />
+        <link rel="apple-touch-icon" sizes="60x60" href="/faviconsWithBg.ico/apple-icon-60x60.png" />
+        <link rel="apple-touch-icon" sizes="72x72" href="/faviconsWithBg.ico/apple-icon-72x72.png" />
+        <link rel="apple-touch-icon" sizes="76x76" href="/faviconsWithBg.ico/apple-icon-76x76.png" />
+        <link rel="apple-touch-icon" sizes="114x114" href="/faviconsWithBg.ico/apple-icon-114x114.png" />
+        <link rel="apple-touch-icon" sizes="120x120" href="/faviconsWithBg.ico/apple-icon-120x120.png" />
+        <link rel="apple-touch-icon" sizes="144x144" href="/faviconsWithBg.ico/apple-icon-144x144.png" />
+        <link rel="apple-touch-icon" sizes="152x152" href="/faviconsWithBg.ico/apple-icon-152x152.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/faviconsWithBg.ico/apple-icon-180x180.png" />
 
         <link rel="icon" type="image/png" sizes="192x192" href="/faviconsNoBg/android-icon-192x192.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/faviconsNoBg/favicon-32x32.png" />
@@ -33,6 +33,7 @@ export default function Home() {
         <main>
           <Header />
           <StartBlock />
+          <img src='./faviconsWithBg.ico.ico/apple-icon-180x180.png' />
           <PopularBlock />
           <Footer />
         </main>
