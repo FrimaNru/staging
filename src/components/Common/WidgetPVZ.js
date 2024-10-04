b3a3c722a36b5a69e2a99d789b487b0e19eec645
FrimaNru <100356059+FrimaNru@users.noreply.github.com>
2024-10-04 21:41:01 +0300

    upd

--- src/components/Common/WidgetPVZ.js
+++ src/components/Common/WidgetPVZ.js
@@ -13,17 +13,22 @@ const WidgetPVZ = ({ onSelectPVZ }) => {
     try {
       const widget = new CdekWidget({
         element: widgetRef.current,
-        servicepath: 'https://widget.cdek.ru/widget/scripts/',
-        apikey: 'oq6SGTH2JEszUlewiFNSi9DHagsfMviF', // Замените на ваш фактический API-ключ СДЭК
+        root: 'cdek-map',
+        servicePath: 'https://api.mi-alegria.shop/map_service/service.php',
+        apiKey: 'a2ab5825-bf63-4a48-b7dc-c03fd2fe6ebf',
+        // apikey: 'oq6SGTH2JEszUlewiFNSi9DHagsfMviF', // Замените на ваш фактический API-ключ СДЭК
         goods: [{ weight: 0.5, height: 10, width: 10, length: 10 }],
         defaultLocation: 'Москва', // Передача города как строки
         map: {
           provider: 'yandex',
           yandex: {
-            apiKey: 'a2ab5825-bf63-4a48-b7dc-c03fd2fe6ebf', // Ваш API-ключ Яндекс.Карт
             center: [55.7558, 37.6173],
             zoom: 10,
           },
+        },
+        hideDeliveryOptions: {
+          office: false,
+          door: false,
         },
         onChoose: (pvz) => {
           onSelectPVZ(pvz);
