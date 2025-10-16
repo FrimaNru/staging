35fc994f6018175ad7ac64e32dc8bec71a8d2070
Retsuky <zerokay.q@gmail.com>
2025-10-16 23:12:50 +0300

    vse otkatil

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,40 +1,41 @@
-import '../styles/globals.css';
-import { ChakraProvider } from '@chakra-ui/react';
+import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import '@fontsource/tenor-sans';
-import '@fontsource/dela-gothic-one';
-import { useRouter } from 'next/router';
-import { useEffect } from 'react';
-import { CartProvider } from '@/contexts/CartContext';
-import { FavouriteProvider } from '@/contexts/FavouriteContext';
-import { UserProvider } from '@/contexts/UserContext';
-import { ProductsProvider } from '@/contexts/ProductsContext';
+import "@fontsource/tenor-sans";
+import "@fontsource/dela-gothic-one";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { CartProvider } from "@/contexts/CartContext";
+import { FavouriteProvider } from "@/contexts/FavouriteContext";
+import { UserProvider } from "@/contexts/UserContext";
+import { ProductsProvider } from "@/contexts/ProductsContext";
 
 export default function App({ Component, pageProps }) {
+    const router = useRouter();
 
-  const router = useRouter();
+    useEffect(() => {
+        const handleRouteChange = (url) => {
+            sessionStorage.setItem("prevPath", window.location.href);
+        };
 
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      sessionStorage.setItem('prevPath', window.location.href);
-    };
+        router.events.on("routeChangeStart", handleRouteChange);
+        return () => {
+            router.events.off("routeChangeStart", handleRouteChange);
+        };
+    }, []);
 
-    router.events.on('routeChangeStart', handleRouteChange);
-    return () => {
-      router.events.off('routeChangeStart', handleRouteChange);
-    };
-  }, []);
-
-  return <CartProvider>
-    <ProductsProvider>
-      <UserProvider>
-        <FavouriteProvider>
-          <ChakraProvider>
-            <Component {...pageProps} />
-          </ChakraProvider>
-        </FavouriteProvider>
-      </UserProvider>
-    </ProductsProvider>
-  </CartProvider>
+    return (
+        <CartProvider>
+            <ProductsProvider>
+                <UserProvider>
+                    <FavouriteProvider>
+                        <ChakraProvider>
+                            <Component {...pageProps} />
+                        </ChakraProvider>
+                    </FavouriteProvider>
+                </UserProvider>
+            </ProductsProvider>
+        </CartProvider>
+    );
 }
--- src/pages/auth/yandex/callback.js
+++ src/pages/auth/yandex/callback.js
@@ -1,16 +1,14 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { useRouter } from "next/router";
+import axios from "axios";
 import { API_BASE_URL } from "../../../../apiConfig";
 import { useUser } from "../../../contexts/UserContext";
 
 export default function YandexCallback() {
     const router = useRouter();
     const { setUser } = useUser();
-    const handledRef = useRef(false);
 
     useEffect(() => {
-        if (handledRef.current) return;
-        handledRef.current = true;
         const handleYandexCallback = async () => {
             const urlParams = new URLSearchParams(window.location.search);
             const code = urlParams.get("code");
@@ -31,19 +29,36 @@ export default function YandexCallback() {
             }
 
             if (code) {
-                // Топ-уровневая навигация на API-колбэк без CORS
-                window.location.replace(
-                    `${API_BASE_URL}auth/yandex/callback?code=${encodeURIComponent(
+                try {
+                    console.log(
+                        "Sending Yandex callback request with code:",
                         code
-                    )}`
-                );
-                return;
+                    );
+                    const response = await axios.get(
+                        `${API_BASE_URL}auth/yandex/callback?code=${code}`
+                    );
+                    console.log("Yandex callback response:", response.data);
+
+                    if (response.data.token) {
+                        localStorage.setItem("token", response.data.token);
+                        setUser(response.data.data);
+                        console.log("User set in context:", response.data.data);
+                        router.push("/cabinet?page=personaldata");
+                    } else {
+                        console.error("No token received from Yandex callback");
+                        router.push("/");
+                    }
+                } catch (error) {
+                    console.error("Yandex callback error:", error);
+                    router.push("/");
+                }
+            } else {
+                router.push("/");
             }
-            router.push("/");
         };
 
         handleYandexCallback();
-    }, [router, setUser]);
+    }, [router]);
 
     return (
         <div
